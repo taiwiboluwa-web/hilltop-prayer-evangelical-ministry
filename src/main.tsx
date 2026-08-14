56554e72d8f12aba181d79fd172a67fafaa43610
Taiwo Boluwatife <taiwiboluwa@gmail.com>
2026-08-14 21:36:07 +0100

    Add location and directions section to site

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { Analytics } from './components/Analytics'
+import { LocationDirections } from './components/LocationDirections'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Analytics />
     <App />
+    <LocationDirections />
   </React.StrictMode>,
 )
