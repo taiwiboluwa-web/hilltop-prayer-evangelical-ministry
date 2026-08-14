55615238400c34efad65d1284cc024c94103975a
Taiwo Boluwatife <taiwiboluwa@gmail.com>
2026-08-14 06:15:40 +0100

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
+      
+      {/* Rest of your page content goes here */}
+      <main>
+        {/* Hero section, etc. */}
+      </main>
+    </div>
+  );
+}
