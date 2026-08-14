82fdb4c431836456098f2cc887ec8edd5c4ea9c5
Taiwo Boluwatife <taiwiboluwa@gmail.com>
2026-08-14 21:32:29 +0100

    Enable analytics and webmaster verification integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { Analytics } from './components/Analytics'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>,
 )
