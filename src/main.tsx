ca386b3d063220f05a90e62a9f2c691b45b55784
Taiwo Boluwatife <taiwiboluwa@gmail.com>
2026-08-14 21:39:34 +0100

    Place location section before footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
+import { createPortal } from 'react-dom'
 import './index.css'
 import App from './App'
 import { Analytics } from './components/Analytics'
 import { LocationDirections } from './components/LocationDirections'
 
+function Site() {
+  const [locationHost, setLocationHost] = useState<HTMLElement | null>(null)
+
+  useLayoutEffect(() => {
+    const footer = document.querySelector('footer')
+    if (!footer?.parentNode) return
+
+    const host = document.createElement('div')
+    host.id = 'location-host'
+    footer.parentNode.insertBefore(host, footer)
+    setLocationHost(host)
+
+    return () => {
+      host.remove()
+    }
+  }, [])
+
+  return (
+    <>
+      <Analytics />
+      <App />
+      {locationHost && createPortal(<LocationDirections />, locationHost)}
+    </>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Analytics />
-    <App />
-    <LocationDirections />
+    <Site />
   </React.StrictMode>,
 )
